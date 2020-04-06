395abaa8eec2c2b0be30e1a5bf7c888edffd761f
WelderMileu <welderfelix21@gmail.com>
2020-04-05 22:57:02 -0300

    Adicionando o google maps na aplicação

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Geolocalização com Javascript vanila.
 
 - Usando a api navigator do browser.
+
+### OBS
+
+- A Chave do *google maps* não esta funcionando pois não concluir o meu cadastro gratuito no google *maps development*.
--- main.js
+++ main.js
@@ -3,22 +3,38 @@
 
 	const $ = elem => document.querySelector(elem);
 
-	function localizar() {
-		if ('geolocation' in navigator) {
-			const posisao = navigator.geolocation.getCurrentPosition(function(position) {
-				const obj = {
-					latitude: position.coords.latitude, 
-					longitude : position.coords.longitude
-				};
+		navigator.geolocation.getCurrentPosition(function(e) {
+			const position = {
+				lat: e.coords.latitude,
+				lng: e.coords.longitude
+			}
 
-				$('.latitude-text').innerHTML = `${obj.latitude}`;
-				$('.longitude-text').innerHTML = `${obj.longitude}`;
-				console.log(obj)
-			});
-		} else {
-			return false;
+			console.log(position);
+			
+			const lat = document.createElement('p');
+			const lng = document.createElement('p');
+
+			lat.innerHTML = position.lat;
+			lng.innerHTML = position.lng;
+
+			$('.location').appendChild(lat);
+			$('.location').appendChild(lng);
+			
+			$('.location').style.textAlign = 'center';
+			$('.location').style.marginTop = '20px';
+		})
+
+	function initMap() {
+		const leng = {
+			lat: -19.8975,
+			lng: -43.9625
 		}
-	}
 
-	localizar()
+		const options = {
+			center: leng,
+			zoom: 13
+		}
+
+		const map = new google.maps.Map(document.getElementById('map'), options);
+	}
 })();
