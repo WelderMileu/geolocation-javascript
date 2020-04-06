8bd731beb5f62ae933a086b3aeadebe7c4660512
WelderMileu <welderfelix21@gmail.com>
2020-04-05 23:45:00 -0300

    Organizando a aplicação

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 
 	const $ = elem => document.querySelector(elem);
 
+	function setPosition() {
+
 		navigator.geolocation.getCurrentPosition(function(e) {
 			const position = {
 				lat: e.coords.latitude,
@@ -10,31 +12,17 @@
 			}
 
 			console.log(position);
-			
+				
 			const lat = document.createElement('p');
 			const lng = document.createElement('p');
 
-			lat.innerHTML = position.lat;
-			lng.innerHTML = position.lng;
+			lat.innerHTML = `<b>Latitude:</b>  ${position.lat}`;
+			lng.innerHTML = `<b>Longitude:</b> ${position.lng}`;
 
 			$('.location').appendChild(lat);
 			$('.location').appendChild(lng);
-			
-			$('.location').style.textAlign = 'center';
-			$('.location').style.marginTop = '20px';
-		})
-
-	function initMap() {
-		const leng = {
-			lat: -19.8975,
-			lng: -43.9625
-		}
-
-		const options = {
-			center: leng,
-			zoom: 13
-		}
-
-		const map = new google.maps.Map(document.getElementById('map'), options);
+		});
 	}
+
+	setPosition();
 })();
